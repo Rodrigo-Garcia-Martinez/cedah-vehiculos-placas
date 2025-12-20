3579a815404b788f0802d2cce50658686eb6e646
Rodrigo García Martínez <rodrigo.garcia.mtz33@gmail.com>
2025-12-19 22:18:33 -0600

    captcha2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Sistema de Vehículos",
+  title: "Información de Transporte",
   description: "Gestión de vehículos con autenticación",
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -211,7 +211,7 @@ export default function Home() {
 
             {/* Validar Placa */}
             <div className="mt-6 p-6 bg-gray-100 rounded-lg">
-              <h5 className="text-center mb-4 flex items-center justify-center gap-2 text-lg font-semibold">
+              <h5 className="text-center mb-4 flex items-center justify-center gap-2 text-lg font-semibold text-[20px]">
                 {/* <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                   <path d="M9 9a2 2 0 114 0 2 2 0 01-4 0z" />
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a4 4 0 00-3.446 6.032l-2.261 2.26a1 1 0 101.414 1.415l2.261-2.261A4 4 0 1011 5z" clipRule="evenodd" />
